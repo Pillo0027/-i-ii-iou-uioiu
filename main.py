2973ceeca04407f62493f5795e2718c12a606dfc
Pillo0027 <emiliobatu27@gmail.com>
2026-03-13 18:57:16 -0600

    abc

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-print("Hello world")
+def pri():
+    print("Bienvenido, para registrarse ingrese usuario y cree una contraseña.")
+    us = input("Ingrese el usuario: ")
+    con = input("Ingrese su contraseña: ")
+    con2 = input("Repita la contraseña: ")
+    
+    if con == con2:
+        print("Las contraseñas han sido validadas, puede continuar")
+        return us, con
+    else:
+        print("Contraseñas inválidas, el programa se reiniciará")
+        return pri()
 
-def haz_x_cosa():
-    print("x cosa")
+def pinpon(us, con):
+    while True:
+        print("Bienvenido, para entrar ingrese su contraseña y usuario")
+        us_ans = input("Ingrese su usuario: ")
+        con_ans = input("Ingrese su contraseña: ")
 
+        if us == us_ans and con == con_ans:
+            print("Has ingresado correctamente")
+            break
+        elif us == us_ans and con != con_ans:
+            print("Contraseña incorrecta, se redirige al inicio del programa")
+            break
+        elif us != us_ans and con == con_ans:
+            print("Usuario incorrecto, se redirige al inicio del programa")
+            break
+
+        elif us != us_ans and con != con_ans:
+            print("Usuario y contraseña incorrectos, se redirige al inicio del programa")
+            break
+        
+        err = input("¿Desea continuar con el código? (poner 'continuar' para seguir, 'exit' para salir): ")
+        if err == "exit":
+            break
+
+usr, contraseña = pri()
+pinpon(usr, contraseña)
+print("Programa finalizado")
